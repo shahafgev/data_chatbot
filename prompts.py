b1c12e8845d0bb780bb4e041623570c360c3d4db
shahaf gev <gevs@post.bgu.ac.il>
2023-08-14 14:24:08 +0300

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 QUALIFIED_TABLE_NAME = "FROSTY_SAMPLE.NBA.PER_GAME_STATS"
 TABLE_DESCRIPTION = """
+
 This view contains various statistics for NBA players during the 2021-2022 season, representing averages per game.
 The statistics include player positions, team affiliations, games played, minutes played, field goals, three-pointers,
 free throws, rebounds, assists, steals, blocks, turnovers, personal fouls, and points.
@@ -27,8 +28,8 @@ Here are 7 critical rules for the interaction you must abide:
 4. Make sure to generate a single Snowflake SQL code snippet, not multiple. 
 5. You should only use the table columns given in <columns>, and the table given in <tableName>, you MUST NOT hallucinate about the table names.
 6. DO NOT put numerical at the very front of SQL variable.
-7. YOU SHOULD ALWAYS WRAP THE COLUMNS NAMES WITH "" WHEN WRITING A QUERY
 </rules>
+7. YOU SHOULD ALWAYS WRAP THE COLUMNS NAMES WITH "" WHEN WRITING A QUERY
 
 Don't forget to use "ilike %keyword%" for fuzzy match queries (especially for variable_name column)
 and wrap the generated sql code with ``` sql code markdown in this format e.g:
@@ -57,7 +58,37 @@ def get_table_context(table_name: str, table_description: str):
     column_descriptions = {
         "PLAYER": "The player name",
         "POS": "Player position (C = center, PF = power forward, SF = small forward, SG = shooting guard, PG = point guard)",
-        "TM": "Team affiliation",
+        "TM": "Team affiliation, "
+              """   ATL: Atlanta Hawks - City: Atlanta, State: Georgia, Conference: Eastern Conference
+                    BOS: Boston Celtics - City: Boston, State: Massachusetts, Conference: Eastern Conference
+                    BRK: Brooklyn Nets - City: Brooklyn, State: New York, Conference: Eastern Conference
+                    CHA: Charlotte Hornets - City: Charlotte, State: North Carolina, Conference: Eastern Conference
+                    CHI: Chicago Bulls - City: Chicago, State: Illinois, Conference: Eastern Conference
+                    CLE: Cleveland Cavaliers - City: Cleveland, State: Ohio, Conference: Eastern Conference
+                    DAL: Dallas Mavericks - City: Dallas, State: Texas, Conference: Western Conference
+                    DEN: Denver Nuggets - City: Denver, State: Colorado, Conference: Western Conference
+                    DET: Detroit Pistons - City: Detroit, State: Michigan, Conference: Eastern Conference
+                    GSW: Golden State Warriors - City: San Francisco, State: California, Conference: Western Conference
+                    HOU: Houston Rockets - City: Houston, State: Texas, Conference: Western Conference
+                    IND: Indiana Pacers - City: Indianapolis, State: Indiana, Conference: Eastern Conference
+                    LAC: LA Clippers - City: Los Angeles, State: California, Conference: Western Conference
+                    LAL: Los Angeles Lakers - City: Los Angeles, State: California, Conference: Western Conference
+                    MEM: Memphis Grizzlies - City: Memphis, State: Tennessee, Conference: Western Conference
+                    MIA: Miami Heat - City: Miami, State: Florida, Conference: Eastern Conference
+                    MIL: Milwaukee Bucks - City: Milwaukee, State: Wisconsin, Conference: Eastern Conference
+                    MIN: Minnesota Timberwolves - City: Minneapolis, State: Minnesota, Conference: Western Conference
+                    NOP: New Orleans Pelicans - City: New Orleans, State: Louisiana, Conference: Western Conference
+                    NYK: New York Knicks - City: New York City, State: New York, Conference: Eastern Conference
+                    OKC: Oklahoma City Thunder - City: Oklahoma City, State: Oklahoma, Conference: Western Conference
+                    ORL: Orlando Magic - City: Orlando, State: Florida, Conference: Eastern Conference
+                    PHI: Philadelphia 76ers - City: Philadelphia, State: Pennsylvania, Conference: Eastern Conference
+                    PHO: Phoenix Suns - City: Phoenix, State: Arizona, Conference: Western Conference
+                    POR: Portland Trail Blazers - City: Portland, State: Oregon, Conference: Western Conference
+                    SAC: Sacramento Kings - City: Sacramento, State: California, Conference: Western Conference
+                    SAS: San Antonio Spurs - City: San Antonio, State: Texas, Conference: Western Conference
+                    TOR: Toronto Raptors - City: Toronto, Province: Ontario, Conference: Eastern Conference
+                    UTA: Utah Jazz - City: Salt Lake City, State: Utah, Conference: Western Conference
+                    WAS: Washington Wizards - City: Washington, D.C., Conference: Eastern Conference """,
         "AGE": "The player's age",
         "G": "Games played",
         "GS": "Games started",
